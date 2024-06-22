ebb6e46bc16b48645bf6bb4343c2b40f2f0b90ec
Chido100 <chido_dozie@hotmail.com>
2024-06-22 21:31:12 +0100

    Install Next.js

--- core_apps/users/admin.py
+++ core_apps/users/admin.py
@@ -25,6 +25,7 @@ class UserAdmin(BaseUserAdmin):
     )
 
     add_fieldsets = ((None,{"classes": ("wide",), "fields": ("username", "email", "first_name", "last_name", "password1", "password2",),},),)
+    
 
 
 
