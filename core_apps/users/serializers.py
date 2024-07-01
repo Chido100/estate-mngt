17fb36e93308e42896c40c3b9a42197d1eb59cdb
Chido100 <chido_dozie@hotmail.com>
2024-07-01 11:01:17 +0100

    customize djoser current user serializer

--- core_apps/users/serializers.py
+++ core_apps/users/serializers.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import get_user_model
-from djoser.serializers import UserCreateSerializer
+from djoser.serializers import UserCreateSerializer, UserSerializer
+from django_countries.serializer_fields import CountryField
+from phonenumber_field.serializerfields import PhoneNumberField
+from rest_framework import serializers
 
 
 User = get_user_model()
@@ -8,5 +11,23 @@ class CreateUserSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ["id", "username", "first_name", "last_name", "password"]
+
+
+class CustomUserSerializer(UserSerializer):
+    full_name = serializers.ReadOnlyField(source="get_full_name")
+    gender = serializers.ReadOnlyField(source="profile.gender")
+    slug = serializers.ReadOnlyField(source="profile.slug")
+    occupation = serializers.ReadOnlyField(source="profile.occupation")
+    phone_number = PhoneNumberField(source="profile.phone_number")
+    avatar = serializers.ReadOnlyField(source="profile.avatar.url")
+
+    class Meta(UserSerializer.Meta):
+        model = User
+        fields = [
+            "id", "email", "first_name", "last_name", "username", "slug", "full_name",
+            "gender", "occupation", "phone_number", "avatar", "date_joined",
+        ]
+        read_only_fields=["id", "email", "date_joined"]
+
 
         
